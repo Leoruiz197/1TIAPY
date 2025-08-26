316019c10119b9cf4023f8d6eca58b13bb7d4dcb
William-Furquim <william.furquim47@gmail.com>
2025-08-26 20:28:36 -0300

    Nova Atualização William Furqui,

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from leituradedados import ler_dados_csv
-from calculovendas import calcular_total_vendas
+from leitura_de_dados import ler_dados_csv
+from calculo_vendas import calcular_total_vendas
+from filtragem_por_produto import filtrar_vendas_por_produto
+
 
 dados = ler_dados_csv('vendas.csv')
 total = calcular_total_vendas(dados, chave_valor='valor')
