8ebbdc4de87305adb3159546b310e834af566382
biellpessoa <biel67489@gmail.com>
2025-08-26 20:14:52 -0300

    Realizado exercicio 1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Meu primeiro codigo python no git')
+import csv
+from typing import List, Dict
+
+caminho_arquivo = "dados.csv"
+
+def ler_dados_csv(caminho_arquivo) -> List[Dict[str, str]]:
+
+    with open(caminho_arquivo, newline="", encoding="utf-8", errors="replace") as f:
+        reader = csv.DictReader(f, delimiter=",")
+        return [dict(row) for row in reader]
+
+# Chamada da função
+dados = ler_dados_csv(caminho_arquivo)
+
+# Visualizar resultado
+print(dados)
