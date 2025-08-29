880cc8d835e3c59eab17e1cd1922492473abe74c
leoruiz197 <ruizruizleo@hotmail.com>
2025-08-29 19:55:14 -0300

    ex 04 resolvido

--- ex04.py
+++ ex04.py
@@ -1,2 +1,9 @@
-def gravar_resultados():
-    print("Implementar")
+import csv
+
+def gravar_resultados(total_vendas,vendas_filtradas):
+    with open('resultado_vendas.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([f"Total de vendas: R${total_vendas:.2f}"])
+        writer.writerow(["Vendas filtradas:"])
+        for item in vendas_filtradas:
+            writer.writerow([item])
